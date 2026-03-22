ec1d8e4a70f44cfd7db7260cdfb71e045d467fb7
lukacho123 <119809931+lukacho123@users.noreply.github.com>
2026-03-22 08:23:12 +0400

    Revert "fix mobile menu"
    This reverts commit 3d3999dc59f61097a7467b2f5853d1f0ef390d38.

--- basketscript.js
+++ basketscript.js
@@ -9,12 +9,3 @@ function menutoggle() {
         menu.style.maxHeight = '0px';
     }
 }
-
-function menutoggle() {
-    const menu = document.getElementById('MenuItems');
-    if (menu.style.maxHeight == '0px' || menu.style.maxHeight == '') {
-        menu.style.maxHeight = '200px';
-    } else {
-        menu.style.maxHeight = '0px';
-    }
-}
